1798445666a39b2ee99d1130af81e3c63d17f7a1
Nathan Turi <turinath@msu.edu>
2022-08-25 13:23:32 -0400

    Initial commit

---
+++ PCTesterMP.py
@@ -0,0 +1,150 @@
+import numpy as np
+from scipy.signal import find_peaks
+from scipy.integrate import trapezoid
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.graph_objects as go
+import time
+from multiprocessing import Pool, cpu_count
+import sys
+sys.path.insert(0, 'TPC-utils')
+from tpc_utils import background, search_high_res
+from TPCH5_utils import get_first_last_event_num, load_trace
+
+def deconv(traces):
+	return search_high_res(traces, sigma = 4, threshold = 60, remove_bkg = True, number_it = 200, markov = True, aver_window = 5)[0]
+
+
+if __name__ == '__main__':
+	PATH = '/mnt/research/attpc/e20009/h5/run_0231.h5'
+
+	first_event_num, last_event_num = get_first_last_event_num(PATH)
+
+	zap_pads = np.loadtxt('Zap_pads.csv', skiprows = 2, delimiter = ',')
+
+	padxy = np.loadtxt('padxy.csv', delimiter = ',', skiprows = 1)
+
+	pad_loc = pd.read_csv('flutsize.csv')
+	pad_big = pad_loc[pad_loc['size'] == 1]
+	pad_small = pad_loc[pad_loc['size'] == 0]
+
+	#event_ind = first_event_num+2
+	event_ind = 1005
+
+	# Picks out the traces from the first event.
+	meta, all_traces = load_trace(PATH, event_ind)
+	all_traces = all_traces.astype(np.float64)
+
+	# Replaces the first and last values in each trace.
+	all_traces[:, 0] = all_traces[:, 1]
+	all_traces[:, -1] = all_traces[:, -2]
+
+	trim_ct = False
+
+	if trim_ct == True:
+    		all_traces, meta = remove_ct(all_traces, meta, threshold1 = 2000, threshold2 = 1600, channel_width = 3, tb_range = 7)
+
+	all_peaks = np.array([])
+	all_energies = np.array([])
+	all_x = np.array([])
+	all_y = np.array([])
+	all_pad_nums = np.array([])
+
+	cores = cpu_count()
+	print('Using ', cores, ' cores for deconvolution.')
+	all_traces_parts = np.array_split(all_traces, cores, axis = 0)
+
+	with Pool(cores) as p:
+		parts = p.map(deconv, all_traces_parts)
+		response = np.vstack(parts)
+
+	#response, _ = search_high_res(all_traces, 
+					#sigma = sig, 
+					#threshold = 60, 
+					#remove_bkg = True, 
+					#number_it = 200, 
+					#markov = True, 
+					#aver_window = 5)
+
+	for trace_num in range(len(all_traces)):
+
+    		sig = 4
+
+    		peaks, _ = find_peaks(response[trace_num], height = 70)
+    		peaks = peaks[np.argsort(response[trace_num][peaks])[::-1]][:]
+
+    		if len(peaks) != 0:
+        		num_pts = int(np.round(sig))
+        
+        		energies = np.array([])
+        
+        		# Loop calculates the energy for each peak
+        		for peak in peaks:
+            			if (((peak+num_pts) < len(response)) and ((peak-num_pts) > 0)):
+                			extra_pts = np.arange(peak-num_pts, peak+num_pts, dtype = int)
+                
+            			energies = np.append(energies, trapezoid(response[trace_num][extra_pts], extra_pts))
+            			#all_x = np.append(all_x, padxy['x'][meta[:,4][trace_num]])
+            			all_x = np.append(all_x, padxy[:,0][meta[:,4][trace_num]])
+            			#all_y = np.append(all_y, padxy['y'][meta[:,4][trace_num]])
+            			all_y = np.append(all_y, padxy[:,1][meta[:,4][trace_num]])
+            			all_pad_nums = np.append(all_pad_nums, meta[:,4][trace_num])
+        
+        		all_peaks = np.append(all_peaks, peaks)
+        		all_energies = np.append(all_energies, energies)
+        
+	drift_vel = 333.076 # In time bucket difference
+	#all_z = all_peaks / np.shape(all_traces)[1] * drift_vel
+	all_z = all_peaks / np.shape(all_traces)[1] * 1000
+
+	pc = np.stack((all_x, all_y, all_z, all_energies, all_pad_nums)).T
+
+	# Beam region
+	energy_thresh_br = 500
+
+	#pc_br = pc[np.isin(all_pad_nums, zap_pads['pad num'])]
+	pc_br = pc[np.isin(all_pad_nums, zap_pads[:,4])]
+	pc_br = pc_br[np.where(pc_br[:,3] >= energy_thresh_br)]
+
+	# Not beam region
+	#energy_thresh_nbr = 1000
+	energy_thresh_nbr_big = 4000
+	energy_thresh_nbr_small = 4000
+
+	#pc_nbr = pc[~np.isin(all_pad_nums, zap_pads['pad num'])]
+	pc_nbr = pc[~np.isin(all_pad_nums, zap_pads[:,4])]
+	#pc_nbr = pc_nbr[np.where(pc_nbr[:,3] >= energy_thresh_nbr)]
+	pc_nbr_big = pc_nbr[np.where(np.isin(pc_nbr[:,4], pad_big))]
+	pc_nbr_big = pc_nbr_big[np.where(pc_nbr_big[:,3] >= energy_thresh_nbr_big)]     
+	pc_nbr_small = pc_nbr[np.where(np.isin(pc_nbr[:,4], pad_small))]
+	pc_nbr_small = pc_nbr_small[np.where(pc_nbr_small[:,3] >= energy_thresh_nbr_small)]
+	pc_nbr = np.vstack((pc_nbr_big, pc_nbr_small))
+	# Drops pads where there are more than 4 points. Consider them "noisy"
+	pads_to_drop = np.unique(pc_nbr[:,4])[np.where(np.array([list(pc_nbr[:,4]).count(i) for i in np.unique(pc_nbr[:,4])]) >= 5)]
+	pc_nbr = pc_nbr[~np.isin(pc_nbr[:,4], pads_to_drop)]
+
+	# Recombine point cloud
+	pc = np.vstack((pc_br, pc_nbr))
+
+	#print('Time it takes to filter the point cloud: ', time.time()-start)
+
+	interact = False
+
+	if (interact == True):
+    		fig = go.Figure(data=[go.Scatter3d(x=pc[:,0], y=pc[:,1], z=pc[:,2],
+                                       mode='markers', marker = dict(size = 2, color = pc[:,3], colorscale = 'viridis'))])
+    		# Forces all plots to have the same limits
+    		fig.update_layout(scene = dict(xaxis = dict(range = [-292, 292]),
+                                   yaxis = dict(range = [-292, 292]),
+                                   zaxis = dict(range = [0, 1000])))
+    		# Forces all axes to have the same scaling in the plots
+    		fig.update_layout(scene_aspectmode='manual', scene_aspectratio=dict(x=1, y=1, z=1000/292))
+    		fig.show()
+	else:
+		plt.figure(figsize = (8, 8))
+		#plt.scatter(padxy['x'], padxy['y'], c = pad_loc.sort_values(by=['pad'])['size'], alpha = 0.25)
+		plt.scatter(pc[:,0], pc[:,1], c = pc[:,3], cmap = 'seismic', s = 2.5, alpha = 1)
+		plt.xlim(-292, 292)
+		plt.ylim(-292, 292)
+		plt.grid()
+		plt.show()
---
+++ TPCH5_utils.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+import pandas as pd
+
+def get_first_last_event_num(PATH):
+	'''
+	Inputs:
+		PATH            : Path to a specific HDF5 file.
+	
+	Returns:
+		first_event_num : An integer that specifies the first event number in the HDF5 file.
+		last_event_num  : An integer that specifies the last event number in the HDF5 file.
+	'''
+	f = h5py.File(PATH, 'r')
+	
+	meta = f['/meta'].get('meta')
+
+	first_event_num = int(meta[0])
+	last_event_num = int(meta[2])
+
+	f.close()
+	return first_event_num, last_event_num
+
+def load_trace(PATH, event_num = 0):
+	'''
+	Inputs:
+		PATH      : Path to a specified HDF5 file.
+		event_num : The event number that you want to look at.
+
+	Returns:
+        meta      : A 2D array that contains the metadata for each trace (CoBo, AsAd, AGET, channel, pad number)
+		trace     : A 2D array that contains the every trace for the specified event.
+	'''
+	f = h5py.File(PATH, 'r')
+	
+	events = f['/get']
+	
+	#first_event_num, last_event_num = get_first_last_event_num(PATH)
+	#event_num = np.random.randint(low = first_event_num, high = last_event_num+1)
+
+	dataset = events.get('evt'+str(event_num)+'_data')
+	header = events.get('evt'+str(event_num)+'_header')
+	
+	first_event_num, last_event_num = get_first_last_event_num(PATH)
+
+	if (event_num < first_event_num) or (event_num > last_event_num):
+		raise Exception("Event number must be between "+str(first_event_num)+" and "+str(last_event_num)+" (default is 0)")
+
+	CoBo = dataset[:, 0]
+	AsAd = dataset[:, 1]
+	AGET = dataset[:, 2]
+	channel = dataset[:, 3]
+	pad_num = dataset[:, 4]
+	
+	meta = np.transpose(np.array([CoBo, AsAd, AGET, channel, pad_num]))
+
+	trace = dataset[:, 5:]
+	
+	return meta.astype(np.int64), trace.astype(np.int64)
+
+def main():
+	PATH = '/mnt/research/attpc/e20009/h5/run_0231.h5'
+	
+	first_event_num, last_event_num = get_first_last_event_num(PATH)	
+
+	meta, trace = load_trace(PATH, first_event_num)
+
+	print(np.shape(meta))
+	print(np.shape(trace))
+	
+if __name__ == '__main__':
+	main()
+	print('Done!')
