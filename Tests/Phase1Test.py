3ff9ef5babe776fcbe888bdb4f78653a92225058
Nathan Turi <turinath@msu.edu>
2023-08-10 16:35:06 -0400

    Commented out some redundant code in the point_cloud class. Added a test for the GetEvent class.

--- Tests/Phase1Test.py
+++ Tests/Phase1Test.py
@@ -3,13 +3,12 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append('..')
 from pcutils.core.get_trace import GetTrace
+from pcutils.core.get_event import GetEvent, read_get_event
 from pcutils.core.hardware_id import hardware_id_from_array
 from pcutils.hdf.TPCH5_utils import load_trace, get_first_last_event_num
 
-def main():
-    h5_Path = '/mnt/analysis/e20009/e20009_Turi/run_0348.h5'
-
-    metas, traces = load_trace(h5_Path, event_num = 146674)
+def GetTraceTest(h5_Path, event_num):
+    metas, traces = load_trace(h5_Path, event_num = event_num)
 
     for i in range(100):
 
@@ -30,31 +29,35 @@ def main():
     for Peak in Trace.peaks:
         print(Peak.positive_inflection, Peak.negative_inflection, Peak.uncorrected_amplitude)
 
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize = (8, 6))
-    ax1.set_title(trace_num)
-
-    ax1.plot(Trace.raw_data, label = 'Raw data')
-    ax1.grid()
-    ax1.legend()
-
-    ax2.plot(Trace.corrected_data, label = 'Data w/o baseline')
-    ax2.plot(Trace.smoothed_output, label = 'Smoothed data w/o baseline')
-    ax2.scatter(np.array([Peak.centroid for Peak in Trace.get_peaks()]), 
-                np.array([Peak.amplitude for Peak in Trace.get_peaks()]), 
+    plt.figure(figsize = (8, 6))
+    plt.plot(Trace.raw_data, label = 'Raw data')
+    plt.plot(Trace.corrected_data, label = 'Data w/o baseline')
+    #plt.plot(Trace.smoothed_output, label = 'Smoothed data w/o baseline')
+    plt.scatter(np.array([Peak.centroid for Peak in Trace.get_peaks()]),
+                np.array([Peak.amplitude for Peak in Trace.get_peaks()]),
                 marker = '*', 
-                color = 'green', 
+                color = 'green',
                 zorder = 3)
     for Peak in Trace.get_peaks():
-        ax2.axvspan(xmin = Peak.positive_inflection, 
-                    xmax = Peak.negative_inflection, 
-                    ymin = min(Trace.corrected_data), 
+        plt.axvspan(xmin = Peak.positive_inflection,
+                    xmax = Peak.negative_inflection,
+                    ymin = min(Trace.corrected_data),
                     ymax = max(Trace.corrected_data),
                     alpha = 0.5)
-    ax2.grid()
-    ax2.legend()
-
+    plt.grid()
+    plt.legend()
+    plt.title(trace_num)
     plt.show()
 
+def GetEventTest(h5_Path, event_num):
+    Event = read_get_event(h5_Path, event_num)
+    print(Event.name)
+
+def main():
+    h5_Path = '/mnt/analysis/e20009/e20009_Turi/run_0348.h5'
+    event_num = 146674
+    GetTraceTest(h5_Path, event_num)
+    GetEventTest(h5_Path, event_num)
 
 if __name__ == '__main__':
     main()
