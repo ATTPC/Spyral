7b431c277ede9de7e03643d62ba217c61f2c7bf0
Nathan Turi <turinath@msu.edu>
2023-08-11 14:56:45 -0400

    Finished adding test for phase 1 where pulse analysis is done and a point cloud is generated from the traces

--- Tests/Phase1Test.py
+++ Tests/Phase1Test.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 import sys
 sys.path.append('..')
 from pcutils.core.get_trace import GetTrace
 from pcutils.core.get_event import GetEvent, read_get_event
+from pcutils.core.point_cloud import PointCloud
 from pcutils.core.hardware_id import hardware_id_from_array
+from pcutils.core.pad_map import PadMap
 from pcutils.hdf.TPCH5_utils import load_trace, get_first_last_event_num
 
 def GetTraceTest(h5_Path, event_num):
@@ -53,11 +56,34 @@ def GetEventTest(h5_Path, event_num):
     Event = read_get_event(h5_Path, event_num)
     print(Event.name)
 
+def PointCloudTest(h5_Path, event_num):
+    pad_geometry_Path = '../etc/padxy.csv'
+    gain_Path = '../etc/pad_gain_map.csv'
+    time_correction_Path = '../etc/pad_time_correction.csv'
+
+    padmap = PadMap(geometry_path = pad_geometry_Path, gain_path = gain_Path, time_correction_path = time_correction_Path)
+
+    Event = read_get_event(h5_Path, event_num)
+    pc = PointCloud()
+    pc.load_cloud_from_get_event(event = Event, pad_geometry = padmap, peak_separation = 4, peak_threshold = 200)
+    pc.eliminate_cross_talk()
+    pc.calibrate_z_position(micromegas_tb = 17, window_tb = 500, detector_length = 1000)
+
+    plt.figure(figsize = (8, 8))
+    plt.scatter(pc.cloud[:,0], pc.cloud[:,1], s = 1)
+    plt.xlim([-292, 292])
+    plt.ylim([-292, 292])
+    plt.title('After CT Removal')
+    plt.grid()
+    plt.show()
+
 def main():
     h5_Path = '/mnt/analysis/e20009/e20009_Turi/run_0348.h5'
     event_num = 146674
-    GetTraceTest(h5_Path, event_num)
-    GetEventTest(h5_Path, event_num)
+
+    #GetTraceTest(h5_Path, event_num)
+    #GetEventTest(h5_Path, event_num)
+    PointCloudTest(h5_Path, event_num)
 
 if __name__ == '__main__':
     main()
