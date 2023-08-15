a23994d61a57cb75a3127769259be97538c2df7e
Gordon McCann <gmccann@fsu.edu>
2023-08-15 15:13:54 -0400

    Forgot the main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from pcutils.core.config import load_config
 from pcutils.run import run_pcutils
+import sys
 
-def main():
-    config = load_config('config.json')
+def main(config_path: str):
+    config = load_config(config_path)
     run_pcutils(config)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2:
+        print('pcutils requires a configuration file!')
+    else:
+        main(sys.argv[1])
