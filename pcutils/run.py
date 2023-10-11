2561021be86b9ad1a9f818cd62cd4e17ebf963e6
Gordon McCann <mccann@frib.msu.edu>
2023-10-11 14:51:39 -0400

    Update run script to handle jit

--- pcutils/run.py
+++ pcutils/run.py
@@ -5,6 +5,7 @@ from .phase_2 import phase_2
 from .phase_3 import phase_3
 from .phase_4_kalman import phase_4_kalman
 from .phase_4_interp import phase_4_interp
+from .phase_4_interp_jit import phase_4_interp_jit
 from time import time
 
 def run_pcutils(config: Config):
@@ -29,7 +30,8 @@ def run_pcutils(config: Config):
         if config.run.do_phase4:
             # phase_4(idx, ws, config.detector, config.solver)
             # phase_4_kalman(idx, ws, config.detector, config.solver, nuclear_map)
-            phase_4_interp(idx, ws, config.solver, config.detector, nuclear_map)
+            # phase_4_interp(idx, ws, config.solver, config.detector, nuclear_map)
+            phase_4_interp_jit(idx, ws, config.solver, config.detector, nuclear_map)
         
     stop = time()
     print(f'Total ellapsed runtime: {stop - start}s')
