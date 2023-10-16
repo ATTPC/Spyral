305026722adccc92bfac825ad730682c0c32109b
Gordon McCann <mccann@frib.msu.edu>
2023-10-16 16:35:19 -0400

    Fix some name issues after the repo rename. Cleanup some old testing code from the jit update that is no longer needed.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from pcutils.core.config import load_config
-from pcutils.run import run_pcutils
+from spyral.core.config import load_config
+from spyral.run import run
 import sys
 
 def main(config_path: str):
     config = load_config(config_path)
-    run_pcutils(config)
+    run(config)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print('pcutils requires a configuration file!')
+        print('Spyral requires a configuration file!')
     else:
         main(sys.argv[1])
