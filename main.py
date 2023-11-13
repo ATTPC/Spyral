4bac2863e0cf857ea3e0b5bc52bb40ad729f3620
Nathan Turi <turinath@msu.edu>
2023-11-13 15:56:40 -0500

    Made small changes. Readded the timing print at the end of the pipeline. Implimented IC gates in the plotting.

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from spyral.core.config import load_config
 from spyral.run_parallel import run_spyral_parallel
 import sys
 from pathlib import Path
+import time
+import datetime
 
 def main(config_path: str):
     config = load_config(Path(config_path))
     run_spyral_parallel(config)
 
 if __name__ == "__main__":
+    start = time.time()
     if len(sys.argv) < 2:
         print('spyral requires a configuration file!')
     else:
         main(sys.argv[1])
+    print(f'Time elapsed: {time.time()-start} seconds (i.e. {str(datetime.timedelta(seconds = time.time()-start))})')
