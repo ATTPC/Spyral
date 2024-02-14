Fix a host of buggy behavior in clustering phase. Most importantly fix circle area overlap calculation and joining. Add ic multiplicity as value carried. Can now analyze legacy ic data.

spyral/core/clusterize.py
@@ -80,12 +80,12 @@ def join_clusters_step(
         # Reject noise
         if cluster.label == -1 or np.isnan(center[0]) or center[2] < 10.0:
             continue
-        radius: float = np.linalg.norm(center[:2])
+        radius = center[2]
         area = np.pi * radius**2.0
 
         for cidx, comp_cluster in enumerate(clusters):
             comp_center = centers[cidx]
-            comp_radius = np.linalg.norm(comp_center[:2])
+            comp_radius = comp_center[2]
             comp_area = np.pi * comp_radius**2.0
             if (
                 comp_cluster.label == -1
@@ -107,23 +107,30 @@ def join_clusters_step(
             term2 = (center_distance**2.0 + comp_radius**2.0 - radius**2.0) / (
                 2.0 * center_distance * comp_radius
             )
-            term3 = (
-                (-center_distance + radius + comp_radius)
-                * (center_distance + radius - comp_radius)
-                * (center_distance - radius + comp_radius)
-                * (center_distance + radius + comp_radius)
-            )
-            if term3 < 0.0:  # term3 cant be negative, inside sqrt
+            c1 = -center_distance + radius + comp_radius
+            c2 = center_distance + radius - comp_radius
+            c3 = center_distance - radius + comp_radius
+            c4 = center_distance + radius + comp_radius
+            term3 = c1 * c2 * c3 * c4
+            area_overlap = 0.0
+            # term3 cant be negative, inside sqrt.
+            if term3 < 0.0 and c1 < 0.0:
+                # Cannot possibly overlap, too far apart
                 continue
-            term1 = min(
-                1.0, max(-1.0, term1)
-            )  # clamp to arccos range to avoid silly floating point precision errors
-            term2 = min(1.0, max(-1.0, term2))
-            area_overlap = (
-                radius**2.0 * np.arccos(term1)
-                + comp_radius**2.0 * np.arccos(term2)
-                - 0.5 * np.sqrt(term3)
-            )
+            elif term3 < 0.0 and (c2 < 0.0 or c3 < 0.0):
+                # Entirely overlap one circle inside of the other
+                area_overlap = min(area, comp_area)
+            else:
+                # Circles only somewhat overlap
+                term1 = min(
+                    1.0, max(-1.0, term1)
+                )  # clamp to arccos range to avoid silly floating point precision errors
+                term2 = min(1.0, max(-1.0, term2))
+                area_overlap = (
+                    radius**2.0 * np.arccos(term1)
+                    + comp_radius**2.0 * np.arccos(term2)
+                    - 0.5 * np.sqrt(term3)
+                )
 
             smaller_area = min(area, comp_area)
             comp_mean_charge = np.mean(comp_cluster.point_cloud.cloud[:, 4], axis=0)
@@ -206,10 +213,11 @@ def form_clusters(pc: PointCloud, params: ClusterParameters) -> list[LabeledClou
     list[LabeledCloud]
         List of clusters found by the algorithm with labels
     """
-    if len(pc.cloud) < params.min_cloud_size:
-        return []
+
     # Smooth out the point cloud by averaging over neighboring points within a distance, droping any duplicate points
     pc.smooth_cloud(params.smoothing_neighbor_distance)
+    if len(pc.cloud) < params.min_cloud_size:
+        return []
 
     n_points = len(pc.cloud)
     min_size = int(params.min_size_scale_factor * n_points)

spyral/phase_pointcloud_legacy.py
@@ -37,6 +37,7 @@ def phase_pointcloud_legacy(
     ws: Workspace,
     pad_map: PadMap,
     get_params: GetParameters,
+    ic_params: FribParameters,
     detector_params: DetectorParameters,
     queue: SimpleQueue,
 ):
@@ -113,10 +114,15 @@ def phase_pointcloud_legacy(
         except Exception:
             continue
 
-        event = GetLegacyEvent(event_data, idx, get_params)
+        event = GetLegacyEvent(event_data, idx, get_params, ic_params)
 
         pc = PointCloud()
         pc.load_cloud_from_get_event(event, pad_map, corrector)
+        pc.calibrate_z_position(
+            detector_params.micromegas_time_bucket,
+            detector_params.window_time_bucket,
+            detector_params.detector_length,
+        )
 
         pc_dataset = cloud_group.create_dataset(
             f"cloud_{pc.event_number}", shape=pc.cloud.shape, dtype=np.float64
@@ -126,6 +132,19 @@ def phase_pointcloud_legacy(
         pc_dataset.attrs["ic_amplitude"] = -1.0
         pc_dataset.attrs["ic_integral"] = -1.0
         pc_dataset.attrs["ic_centroid"] = -1.0
+        pc_dataset.attrs["ic_multiplicity"] = -1.0
+
+        # Set IC if present; take first non-garbage peak
+        if event.ic_trace is not None:
+            # No way to disentangle multiplicity
+            for peak in event.ic_trace.get_peaks():
+                pc_dataset.attrs["ic_amplitude"] = peak.amplitude
+                pc_dataset.attrs["ic_integral"] = peak.integral
+                pc_dataset.attrs["ic_centroid"] = peak.centroid
+                pc_dataset.attrs["ic_multiplicity"] = (
+                    event.ic_trace.get_number_of_peaks()
+                )
+                break
 
         pc_dataset[:] = pc.cloud
 