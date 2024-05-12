459b4251b2cbbce5d3dcee23778af2b574c5e33f
gwm17 <gordonmccann215@gmail.com>
2024-05-12 08:47:35 -0400

    Expand pad map tests to actually test the map.

--- tests/test_pad.py
+++ tests/test_pad.py
@@ -18,6 +18,18 @@ def test_default_pads():
 
     assert default_pmap.is_valid
 
+    pad0 = default_pmap.get_pad_data(0)
+    assert pad0 is not None
+    assert pad0.x == -269.95294
+    assert pad0.y == 4.2506561
+    assert pad0.gain == 1.3
+    assert pad0.time_offset == 0
+    assert pad0.scale == 1.0
+
+    reverse0 = default_pmap.get_pad_from_hardware(pad0.hardware)
+    assert reverse0 is not None
+    assert reverse0 == 0
+
 
 def test_default_legacy_pads():
     params = PadParameters(
@@ -33,3 +45,15 @@ def test_default_legacy_pads():
     default_pmap = PadMap(params)
 
     assert default_pmap.is_valid
+
+    pad0 = default_pmap.get_pad_data(0)
+    assert pad0 is not None
+    assert pad0.x == -269.95294
+    assert pad0.y == 4.2506561
+    assert pad0.gain == 1.3
+    assert pad0.time_offset == 0
+    assert pad0.scale == 1.0
+
+    reverse0 = default_pmap.get_pad_from_hardware(pad0.hardware)
+    assert reverse0 is not None
+    assert reverse0 == 0
